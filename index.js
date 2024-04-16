218e1d571249b2e7850678dcdc44e0f1af88dac9
Ashish Kumar <ashish.kumar@zopper.com>
2024-04-16 22:44:58 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const isValid = (board, row, col, num) => {
   for (let i = 0; i < 9; i++) {
     if (board[row][i] == num || board[i][col] == num) return false;
@@ -14,7 +13,6 @@ const solve = (arr) => {
   for (let i = 0; i < arr?.length; i++) {
     for (let j = 0; j < arr[0]?.length; j++) {
       if (arr[i][j] == 0) {
-        /* This for loop is to check if the number is valid the location or not*/
         for (let n = 1; n <= 9; n++) {
           if (isValid(arr, i, j, n)) {
             arr[i][j] = n;
@@ -38,10 +36,9 @@ const solve = (arr) => {
   };
 };
 
-const generateHalfSudoku = (req, res) => {
-  let board = Array.from(Array(9)).map(
-    (innerArr) => (innerArr = Array.from(Array(9).fill(0)))
-  );
+const generateSudoku = () => {
+  let nineZeroArr = Array.from(Array(9).fill(0));
+  let board = Array.from(Array(9)).map(() => nineZeroArr);
 
   for (let i = 0; i < 40; i++) {
     let randRow = Math.floor(Math.random() * 8);
@@ -53,16 +50,15 @@ const generateHalfSudoku = (req, res) => {
     }
   }
 
-  res.send({ board });
+  return board;
 };
 
-const getSudokuSolution = (req, res) => {
-  let { board } = req.body;
+const getSudokuSolution = (board) => {
   let solvedVal = solve(board);
-  res.send(solvedVal);
+  return solvedVal;
 };
 
 module.exports = {
   getSudokuSolution,
-  generateHalfSudoku,
+  generateSudoku,
 };
